84765f27acafe01889242903953f04ec62c7bf1f
luanmxz <luanmarcenemxz22@gmail.com>
2024-10-29 19:48:07 -0300

    First commit

---
+++ src/main/java/com/nimble/Know_Gym/KnowGymApplication.java
@@ -0,0 +1,13 @@
+package com.nimble.Know_Gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowGymApplication.class, args);
+	}
+
+}
