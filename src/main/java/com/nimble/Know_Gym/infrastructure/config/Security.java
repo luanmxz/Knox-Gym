419560971992a4d5082ebc10e2f74e1546f2857a
luanmxz <luanmarcenemxz22@gmail.com>
2024-10-30 01:40:33 -0300

    ADD: Created KeyCloakConverter (jwtAuthenticationConverter);

--- src/main/java/com/nimble/Know_Gym/infrastructure/config/Security.java
+++ src/main/java/com/nimble/Know_Gym/infrastructure/config/Security.java
@@ -1,31 +1,19 @@
 package com.nimble.Know_Gym.infrastructure.config;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.nimbusds.jose.shaded.gson.internal.LinkedTreeMap;
+import com.nimble.Know_Gym.infrastructure.config.keycloak.KeyCloakConverter;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @Configuration
 @EnableWebSecurity
@@ -44,33 +32,10 @@ public class Security {
                 .authorizeHttpRequests(
                         (authorize) -> authorize.requestMatchers(HttpMethod.POST, "/api/v1/auth/").permitAll()
                                 .anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(new KeyCloakConverter())));
 
         return http.build();
-    }
-
-    @Bean
-    public JwtAuthenticationConverter jwtAuthenticationConverterForKeycloak() {
-        Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter = jwt -> {
-            Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
-
-            Object client = resourceAccess.get(env.get("KEY_CLOAK_REALM_NAME"));
-
-            @SuppressWarnings("unchecked")
-            LinkedTreeMap<String, List<String>> clientRoleMap = (LinkedTreeMap<String, List<String>>) client;
-
-            List<String> clientRoles = new ArrayList<>(clientRoleMap.get("roles"));
-
-            return clientRoles.stream()
-                    .map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toList());
-        };
-
-        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
-
-        return jwtAuthenticationConverter;
     }
 
     @Bean
---
+++ src/main/java/com/nimble/Know_Gym/infrastructure/config/keycloak/KeyCloakConverter.java
@@ -0,0 +1,55 @@
+package com.nimble.Know_Gym.infrastructure.config.keycloak;
+
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.stereotype.Component;
+
+import com.nimbusds.jose.shaded.gson.internal.LinkedTreeMap;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@Component
+public class KeyCloakConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+    private final Dotenv env = Dotenv.load();
+    private final Converter<Jwt, Collection<GrantedAuthority>> delegate = new JwtGrantedAuthoritiesConverter();
+
+    private List<GrantedAuthority> extractRoles(Jwt jwt) {
+        Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+
+        Object client = resourceAccess.get(env.get("KEY_CLOAK_REALM_NAME"));
+
+        @SuppressWarnings("unchecked")
+        LinkedTreeMap<String, List<String>> clientRoleMap = (LinkedTreeMap<String, List<String>>) client;
+
+        List<String> clientRoles = new ArrayList<>(clientRoleMap.get("roles"));
+
+        return clientRoles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public AbstractAuthenticationToken convert(Jwt jwt) {
+        List<GrantedAuthority> extractedAuthorities = extractRoles(jwt);
+        Collection<GrantedAuthority> authorities = delegate.convert(jwt);
+
+        if (authorities != null) {
+            authorities.addAll(extractedAuthorities);
+        }
+
+        JwtAuthenticationToken token = new JwtAuthenticationToken(jwt, authorities);
+        return token;
+    }
+
+}
